8c22f3851db086a1d9b9027ea3de0410564587ae
Corey Leatherman <coreyleather@gmail.com>
2021-07-26 17:42:47 -0700

    Added game to page instead of console

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,7 +2,6 @@ let cpuWins = 0;
 let yourWins = 0;
 let draws = 0;
 
-
 function randomHand() {
     cpuHand = Math.random()*3;
     if (cpuHand < 1) {
@@ -58,7 +57,7 @@ function game() {
     cpuWins = 0;
     yourWins = 0;
     draws = 0;
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 100; i++) {
         yourHand = prompt("Choose your hand!");
         yourHand = yourHand[0].toUpperCase() + yourHand.slice(1).toLowerCase();
         while (!checkValidHand(yourHand)) {
@@ -69,3 +68,26 @@ function game() {
         console.log(rockPaperScissors(yourHand) + ` The score is ${yourWins}-${cpuWins} and ${draws} draw(s).`);
     }
 }
+
+const btnRock = document.getElementById("btnRock");
+const btnScissors = document.getElementById("btnScissors");
+const btnPaper = document.getElementById("btnPaper");
+const div = document.createElement("div");
+
+function updateRPS(choice) {
+    return function () {
+        div.textContent = rockPaperScissors(choice) + ` The score is ${yourWins}-${cpuWins} and ${draws} draw(s).`;
+        document.body.appendChild(div);
+        if (cpuWins == 5 || yourWins == 5) {
+            win = (cpuWins == 5) ? " I win the match!" : " You win the match!";
+            div.textContent = div.textContent + win;
+            btnRock.removeEventListener("click", updateRPS());
+            btnPaper.removeEventListener("click", updateRPS());
+            btnScissors.removeEventListener("click", updateRPS());
+        }
+    }
+}
+
+btnRock.addEventListener("click", updateRPS("Rock"));
+btnPaper.addEventListener("click", updateRPS("Paper"));
+btnScissors.addEventListener("click", updateRPS("Scissors"));
